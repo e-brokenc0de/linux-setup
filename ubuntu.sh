4b0fa6e1173432a4c45d8fc037c14aff8c563d3f
Brokenc0de <dev@brokenc0de.io>
2024-03-02 23:44:26 +0700

    ok

--- ubuntu.sh
+++ ubuntu.sh
@@ -44,6 +44,8 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 
+antigen theme robbyrussell
+
 # Apply Antigen configuration
 antigen apply
 EOF
